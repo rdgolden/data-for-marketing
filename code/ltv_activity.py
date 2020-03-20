f3b3427e159378e401198deab2f1221b09baced5
Devin Golden <dgolden@trilogyed.com>
2020-03-20 16:07:23 -0400

    files

---
+++ code/ltv_activity.py
@@ -0,0 +1,37 @@
+%matplotlib notebook
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+data = pd.read_csv('./ltv_data.csv')
+
+x = data['Lifetime Value']
+y = data['Age']
+
+def func(x, a, b):
+    return a * x + b
+
+popt, pcov = curve_fit(func, x, y)
+
+plt.clf()
+
+plt.scatter(x, y, label="Original Data")
+plt.plot(x, func(x, *popt), 'r-', label="Fitted Curve")
+
+# Setting the Title, X- and Y-axis labels
+plt.title("Age Vs. LTV")
+plt.ylabel("Age")
+plt.xlabel("Lifetime Value")
+
+# Setting the labeling parameters for the X-axis
+plt.xticks(rotation="vertical")
+
+# Showing  grid lines
+plt.grid(True)
+
+# Showing the legend
+plt.legend()
+
+# Print the resulting bar graph
+plt.show()
